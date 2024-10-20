3bd511e6d8635361da2b77167656920574d01287
Gagahpradana7 <gagahpradana779@gmail.com>
2024-10-20 20:35:29 +0700

    fixing home page description

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm text-wrap text-sm xl:text-base xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+            className="max-w-sm text-wrap text-sm xl:text-base xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-white/90"
           >
             I am a Full Stack Developer eager to learn and grow in the tech
             world. I love building functional and visually appealing web
